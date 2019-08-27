b1e5376584d81d3dd045d6715624185c98494f8f
Glazelf <gideon.steenbergen@gmail.com>
2019-08-27 14:22:13 +0200

    penguin ==> penguin.java

--- java/src/zoop.java
+++ java/src/zoop.java
@@ -55,44 +55,6 @@ public class zoop {
             System.out.println(msg);
         }
 
-        private boolean IsPenguinOneAlive()
-        {
-            return this.penguinOneEnergy > 0;
-        }
-
-        private boolean IsPenguinTwoAlive()
-        {
-            return this.penguinTwoEnergy > 0;
-        }
-
-        private boolean IsPenguinThreeAlive()
-        {
-            return this.penguinThreeEnergy > 0;
-        }
-
-        public boolean IsPolarBearAlive()
-        {
-            return this.polarBearEnergy > 0;
-        }
-
-        private void FeedPenguinOne(int energy)
-        {
-            this.penguinOneEnergy += energy;
-            Say("Penguin 1 snatches a fish.");
-        }
-
-        private void FeedPenguinTwo(int energy)
-        {
-            this.penguinTwoEnergy += energy;
-            Say("Penguin 2 snatches a fish.");
-        }
-
-        private void FeedPenguinThree(int energy)
-        {
-            this.penguinThreeEnergy += energy;
-            Say("Penguin 3 snatches a fish.");
-        }
-
         public void FeedPolarBear(int energy)
         {
             this.polarBearEnergy += energy;
@@ -187,16 +149,6 @@ public class zoop {
             }
         }
 
-        private void ShowNoPenguinToHunt()
-        {
-            Say("There is no penguin left to hunt for the polar bear. You better be careful!");
-        }
-
-        private void ShowNoPenguinFeeded()
-        {
-            Say("The zoo caretaker ate the fish himself.");
-        }
-
         private void ShowGoodBye()
         {
             Say("bye ...");
@@ -213,30 +165,6 @@ public class zoop {
         {
             ShowGoodBye();
             System.exit(0);
-        }
-
-        /**
-         * The zoo caretaker feeds one of the penguins
-         */
-        private void FeedPenguin()
-        {
-            int fish = 50;
-
-            int penguinNumber = GetNumber(1, 3);
-
-            switch(penguinNumber)
-            {
-                case 1:
-                    if(IsPenguinOneAlive() ) FeedPenguinOne(fish); break;
-
-                case 2:
-                    if (IsPenguinTwoAlive()) FeedPenguinTwo(fish); break;
-
-                case 3:
-                    if (IsPenguinThreeAlive()) FeedPenguinThree(fish); break;
-
-                default: ShowNoPenguinFeeded(); break;
-            }
         }
 
         private void FeedPolarBear()
